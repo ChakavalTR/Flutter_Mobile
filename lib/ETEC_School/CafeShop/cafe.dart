a933f4b1bfad6248df01511a8b29bac062193b8e
ChakavalTR <macbookpro@ChakavalTR-2.local>
2024-02-03 22:07:55 +0700

    More work

--- lib/ETEC_School/CafeShop/cafe.dart
+++ lib/ETEC_School/CafeShop/cafe.dart
@@ -13,7 +13,7 @@ class _MydetailState extends State<Mydetail> {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'Category',
+          'Categories',
           style: TextStyle(
             fontSize: 20,
             fontWeight: FontWeight.bold,
