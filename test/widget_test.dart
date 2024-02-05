c4200391487dc2c62460fb00903812d94e61fe02
ChakavalTR <macbookpro@ChakavalTR-2.local>
2024-02-05 22:49:31 +0700

    plus more

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_etec/main.dart';
+import 'package:flutter_etec/demo_main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
